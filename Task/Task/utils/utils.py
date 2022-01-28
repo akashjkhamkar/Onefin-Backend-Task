98e602433256531927b28bb30073b838295f8bd2
akash <akash.khamkar40@gmail.com>
2022-01-28 13:29:34 +0530

    all endpoints done doing refactor
    todo -
    auth

--- Task/Task/utils/utils.py
+++ Task/Task/utils/utils.py
@@ -1,14 +1,48 @@
-from django.http import HttpResponse, JsonResponse
+from django.http import JsonResponse
 import requests
 from requests.auth import HTTPBasicAuth
 import json
 
-from .models import Users, Collections
+from ..models import Users, Collections
+
+USERID = 1
 
 moviesEndpoint = 'https://demo.credy.in/api/v1/maya/movies'
 local_moviesEndpoint = 'http://localhost:8000/movies'
 username = 'iNd3jDMYRKsN1pjQPMRz2nrq7N99q4Tsp9EY9cM0'
 password = 'Ne5DoTQt7p8qrgkPdtenTK8zd6MorcCR5vXZIJNfJwvfafZfcOs4reyasVYddTyXCz9hcL5FGGIVxw3q02ibnBLhblivqQTp4BIC93LZHj4OppuHQUzwugcYu7TIC5H1'
+
+# ----------- helper functions
+
+def favouriteGenre(collections):
+    allgenres = {}
+
+    for c in collections:
+        c = c.toDictionary(True)
+        for movie in c['movies']:
+            if('genres' not in movie):
+                continue
+            
+            movieGenres = movie['genres'].split(',')
+
+            for genre in movieGenres:
+                if genre in allgenres:
+                    allgenres[genre] += 1
+                else:
+                    allgenres[genre] = 1
+
+    favourites = sorted(allgenres, key=allgenres.get, reverse=True)
+    return ','.join(favourites[:3])
+
+def updateHelper(data, collection):
+    if('title' in data and data['title'] is not None):
+        collection.name = data['title']
+    
+    if('description' in data and data['description'] is not None):
+        collection.description = data['description']
+    
+    if('movies' in data and data['movies'] is not None):
+        collection.movies = data['movies']
 
 def urlExchange(response):
     nextlink = response['next']
@@ -36,20 +70,8 @@ def makeRequest(req):
 
     return response    
 
-# --------------------------------------------------------------
 
-def hello(req):
-    return JsonResponse({'msg': 'Hello World!'})
-
-def getMovies(req):
-    response = makeRequest(req)
-
-    if('count' in response):
-        urlExchange(response)
-
-    return JsonResponse(response)
-
-# --------------------------------------------------------------
+# --------------------------------------
 
 def getCollection(req, id):
     collection = Collections.objects.filter(userid=USERID, id=id)
@@ -60,15 +82,6 @@ def getCollection(req, id):
     collection = collection[0].toDictionary(True)
     return JsonResponse(collection)
 
-def updateHelper(data, collection):
-    if('title' in data and data['title'] is not None):
-        collection.name = data['title']
-    
-    if('description' in data and data['description'] is not None):
-        collection.description = data['description']
-    
-    if('movies' in data and data['movies'] is not None):
-        collection.movies = data['movies']
 
 def updateCollection(req, id):
     data = json.loads(req.body)
@@ -95,53 +108,6 @@ def deleteCollection(req, id):
 
     return JsonResponse({'message': f'Deleted the collection {id}'})
 
-def get_update_delete_Collection(req, id):
-    if(req.method == 'GET'):
-        return getCollection(req, id)
-    elif(req.method == 'PUT'):
-        return updateCollection(req, id)
-    elif(req.method == 'DELETE'):
-        return deleteCollection(req, id)
-    else:
-        return JsonResponse({'message': f'No such endpoint {req.method} /collection/id'})
-
-# --------------------------------------------------------------
-
-def favouriteGenre(collections):
-    allgenres = {}
-
-    for c in collections:
-        c = c.toDictionary(True)
-        for movie in c['movies']:
-            if('genres' not in movie):
-                continue
-            
-            movieGenres = movie['genres'].split(',')
-
-            for genre in movieGenres:
-                if genre in allgenres:
-                    allgenres[genre] += 1
-                else:
-                    allgenres[genre] = 1
-
-    favourites = sorted(allgenres, key=allgenres.get, reverse=True)
-    return ','.join(favourites[:3])
-
-def getCollections(req):
-    collections = Collections.objects.filter(userid=USERID)
-    result = {
-        'is_success': True,
-        'data': {
-            'collections': [],
-            'favourite_genres': favouriteGenre(collections)
-        }
-    }
-
-    for c in collections:
-        result['data']['collections'].append(c.toDictionary(False))
-
-    return JsonResponse(result)
-
 def addCollection(req):
     data = json.loads(req.body)
     requiredFields = {'title', 'description', 'movies'}
@@ -159,10 +125,17 @@ def addCollection(req):
     newCollection.save()
     return JsonResponse({'collection_uuid': newCollection.id})
 
-def get_add_Collections(req):
-    if(req.method == 'POST'):
-        return addCollection(req)
-    elif(req.method == 'GET'):
-        return getCollections(req)
-    else:
-        return JsonResponse({'message': f'No such endpoint {req.method} /collection'})
+def getCollections(req):
+    collections = Collections.objects.filter(userid=USERID)
+    result = {
+        'is_success': True,
+        'data': {
+            'collections': [],
+            'favourite_genres': favouriteGenre(collections)
+        }
+    }
+
+    for c in collections:
+        result['data']['collections'].append(c.toDictionary(False))
+
+    return JsonResponse(result)
