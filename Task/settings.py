926df35350ae6b59edeb8e41fa16581ef14888ba
akash <akash.khamkar40@gmail.com>
2022-01-28 19:29:59 +0530

    allowed hosts added

--- Task/settings.py
+++ Task/settings.py
@@ -15,8 +15,6 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-ALLOWED_HOSTS =['*'] 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
@@ -26,7 +24,7 @@ SECRET_KEY = 'django-insecure-m%v$2v$q*aert14w(crs0vx%n&@c62z+4&xnyh^6)gz+_zg8cm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', 'onefin-backend-task-akash.herokuapp.com']
 
 
 # Application definition
